7324b2bbd82eabbb3b8f343f32b12e52975f68c1
chhotu marandi <cmarandi29@gmail.com>
2023-05-24 13:50:13 +0530

    add css file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from 'react-dom'
 import Navbar from "./components/Navbar"
 import Main from './components/Main'
+import './Style.css'
 
 function App () {
  return (
