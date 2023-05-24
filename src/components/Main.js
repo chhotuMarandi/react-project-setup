69a3268e3ba47414702d8001365d074f05a1e30e
chhotu marandi <cmarandi29@gmail.com>
2023-05-24 13:09:35 +0530

    added Navbar and Main Components

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Main () {
+ return (
+  <div>
+     <h2>This is Main section</h2>
+  </div>
+ )
+}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Navbar () {
+ return (
+  <div>
+     <h1>This is header</h1>
+  </div>
+ )
+}
+
+export default Navbar
