906b7ad0a66bd59cd619aa9a8208866b1cfdd5a8
chhotu marandi <cmarandi29@gmail.com>
2023-05-25 15:09:15 +0530

    add style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react"
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import Navbar from "./components/Navbar"
 import Main from './components/Main'
-import './Style.css'
+import './style.css'
 
 function App () {
  return (
-  <div>
+  <div className="app">
     <Navbar />
     <Main />
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import logo from '../logo/react-logo.png'
 
 function Navbar () {
  return (
-  <div>
-     <h1>This is header</h1>
-  </div>
- )
+   <nav className='navbar-section'>
+     <div className='header-section'>
+       <img src={logo} alt='' />
+       <h1>ReactFacts</h1>
+     </div>
+     <div className='navbar-list-section'>
+       <ul className='navbar-list'>
+         <li>React</li>
+         <li>Courses</li>
+         <li>Contribution</li>
+       </ul>
+     </div>
+   </nav>
+ );
 }
 
 export default Navbar
