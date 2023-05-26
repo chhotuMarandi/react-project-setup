6ee536401eed782bbb03b9555d30fd2d653b4b2e
chhotu marandi <cmarandi29@gmail.com>
2023-05-26 15:49:48 +0530

    pass props in a component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Contact () {
+function Contact (props) {
+ console.log(props)
  return (
    <div className='card'>
      <img
-       src='https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg'
+       src={props.img}
        alt=''
      />
-     <h3>Mr. Whiskerson</h3>
-     <p>7845165542</p>
-     <p>cat@gmail.com</p>
+     <h3>{props.name}</h3>
+     <p>{props.number}</p>
+     <p>{props.mail}</p>
    </div>
  );
 }
